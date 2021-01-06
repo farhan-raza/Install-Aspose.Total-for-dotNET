41a6c06108285c601561d81912d303cfb16f79f4
Farhan Raza <33145316+farhan-raza@users.noreply.github.com>
2021-01-07 04:12:37 +0500

    Added Sample Project
    Install or Apply License Aspose.Total

---
+++ Aspose.Total/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Aspose.Total
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetTotalLicense.Run();
+
+            // Work with any of the supported file format here
+            // ....
+        }
+    }
+}
---
+++ Aspose.Total/SetTotalLicense.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.Total
+{
+    public class SetTotalLicense
+    {
+        public static void Run()
+        {
+            // Set license for all APIs one by one
+            Aspose.Words.License WordsLicense = new Aspose.Words.License();
+            WordsLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            // Comment the code if you do not want to set license for any of the API
+            //Aspose.Pdf.License PdfLicense = new Aspose.Pdf.License();
+            //PdfLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Cells.License CellsLicense = new Aspose.Cells.License();
+            CellsLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Email.License EmailLicense = new Aspose.Email.License();
+            EmailLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Slides.License SlidesLicense = new Aspose.Slides.License();
+            SlidesLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Imaging.License ImagingLicense = new Aspose.Imaging.License();
+            ImagingLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.BarCode.License BarcodeLicense = new Aspose.BarCode.License();
+            BarcodeLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Diagram.License DiagramLicense = new Aspose.Diagram.License();
+            DiagramLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Tasks.License TasksLicense = new Aspose.Tasks.License();
+            TasksLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.OCR.License OcrLicense = new Aspose.OCR.License();
+            OcrLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Note.License NoteLicense = new Aspose.Note.License();
+            NoteLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.CAD.License CadLicense = new Aspose.CAD.License();
+            CadLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.ThreeD.License ThreeDLicense = new Aspose.ThreeD.License();
+            ThreeDLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Html.License HtmlLicense = new Aspose.Html.License();
+            HtmlLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Gis.License GisLicense = new Aspose.Gis.License();
+            GisLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Zip.License ZipLicense = new Aspose.Zip.License();
+            ZipLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Page.License PageLicense = new Aspose.Page.License();
+            PageLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.PSD.License PsdLicense = new Aspose.PSD.License();
+            PsdLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.OMR.License Omrlicense = new Aspose.OMR.License();
+            Omrlicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Pub.License PubLicense = new Aspose.Pub.License();
+            PubLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Svg.License SvgLicense = new Aspose.Svg.License();
+            SvgLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Finance.License FinanceLicensed = new Aspose.Finance.License();
+            FinanceLicensed.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Drawing.License DrawingLicense = new Aspose.Drawing.License();
+            DrawingLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+
+            Aspose.Font.License FontLicense = new Aspose.Font.License();
+            FontLicense.SetLicense(@"D:\Test\Aspose.Total.Product.Family.lic");
+        }
+    }
+}
